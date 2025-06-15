862f9c481bfc218b12e1c48e1ac6203d1a0c3aa2
kashifaali <fa21-bcs-106@cuivehari.edu.pk>
2025-06-15 23:23:49 +0500

    axios url change

--- frontend/src/config/redux/axiosClient.js
+++ frontend/src/config/redux/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const clientServer = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://career-sense.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 });
 
